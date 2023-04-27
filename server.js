1df6be26a11a0dbc69ea17c67b24f185e30775a5
RDG97 <ryangann1997@gmail.com>
2023-04-27 16:46:37 +0000

    setting up mongodb

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport')
+const { ObjectID } = require('mongodb');
 
 const app = express();
 
@@ -27,6 +28,39 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please login'
+    });
+  });
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null);
+  });
+});
+
+ // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
+});
+// app.listen out here...
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
